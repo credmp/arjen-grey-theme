95e4a1d66950a08cbce16353b8c1bb0ae137b2ea
Ivan Sokolov <sirikid@openmailbox.org>
2017-10-20 02:28:40 +0300

    Added mode-line-inactive face

--- arjen-grey-theme.el
+++ arjen-grey-theme.el
@@ -36,6 +36,7 @@
  '(cursor ((t (:background "#e1cb8c" ))))
  '(fringe ((t (:background "#2b303a" ))))
  '(mode-line ((t (:foreground "#bdc3ce" :background "#242a34" ))))
+ '(mode-line-inactive ((t (:foreground "#63747c" :background "#242a34" ))))
  '(region ((t (:background "#3c4449" ))))
  '(linum ((t (:foreground  "#595e66" :background "#2a2f38" ))))
  '(secondary-selection ((t (:background "#464a4d" ))))
