dab984d1c557b3fcf20f7d60b7625d5153b45095
Arjen Wiersma <arjen@wiersma.org>
2020-01-15 20:46:57 +0100

    Working on a more elegant structure for the theme

--- arjen-grey-theme.el
+++ arjen-grey-theme.el
@@ -29,62 +29,75 @@
 (deftheme arjen-grey "A soothing dark grey theme by Arjen")
 
 ;; Use http://huecode.com to find some great colors!
-(custom-theme-set-faces
- 'arjen-grey
- ;; Built in faces
- '(default ((t (:foreground  "#bdc3ce" :background "#2a2f38" ))))
- '(cursor ((t (:background "#e1cb8c" ))))
- '(fringe ((t (:background "#2b303a" ))))
- '(mode-line ((t (:foreground "#bdc3ce" :background "#242a34" ))))
- '(region ((t (:background "#3c4449" ))))
- '(linum ((t (:foreground  "#595e66" :background "#2a2f38" ))))
- '(secondary-selection ((t (:background "#464a4d" ))))
- '(font-lock-builtin-face ((t (:foreground "#eacc8c" ))))
- '(font-lock-comment-face ((t (:foreground "#63747c" ))))
- '(font-lock-function-name-face ((t (:foreground "#909fab" ))))
- '(font-lock-keyword-face ((t (:foreground "#b894b0" ))))
- '(font-lock-string-face ((t (:foreground "#a8c194" ))))
- '(font-lock-type-face ((t (:foreground "#a0a5a0" ))))
- '(font-lock-constant-face ((t (:foreground "#8b9db0" ))))
- '(font-lock-variable-name-face ((t (:foreground "#8294ac" ))))
- '(minibuffer-prompt ((t (:foreground "#a8c194" :bold t ))))
- '(font-lock-warning-face ((t (:foreground "red" :bold t ))))
- ;; Helm faces
- '(helm-header ((t (:foreground  "#bdc3ce"
-                    :background "#2a2f38"
-                    :underline nil
-                    :box nil))))
- '(helm-source-header ((t (:foreground  "#bdc3ce"
-                           :background "#2a2f38"
-                           :underline nil
-                           :weight bold
-                           :box (:line-width -1 :style released-button)))))
- '(helm-ff-directory ((t (:foreground  "#bdc3ce"
-                          :background "#2a2f38"
-                          :underline nil
-                          :weight bold
-                          ))))
- '(helm-selection ((t (:background "#3c4449" :underline nil))))
- '(helm-selection-line ((t (:background "#2a2f38"))))
- ;; Perspective faces
- '(persp-selected-face ((t (:foreground  "#eacc8c"))))
- ;; Company mode
- '(company-tooltip ((t (:foreground "#bdc3ce" :background "#242a34"))))
- '(company-tooltip-annotation ((t (:foreground "#eacc8c"))))
- '(company-tooltip-selection ((t (:background "#464a4d"))))
- '(company-tooltip-mouse ((t (:background "#464a4d"))))
- '(company-tooltip-common ((t (:foreground "#909fab"))))
- '(company-scrollbar-fg ((t (:background "#464a4d"))))
- '(company-scrollbar-bg ((t (:background "#242a34"))))
- '(company-preview ((t (:foreground "#bdc3ce" :background "#242a34"))))
- '(company-preview-common ((t (:foreground "#909fab"))))
- ;; Gnus
- '(gnus-header-name ((t (:foreground "#909fab" ))))
- '(gnus-header-content ((t (:foreground "#bdc3ce" ))))
- '(gnus-header-subject ((t (:foreground "#eacc8c" ))))
- '(widget-button ((t (:foreground "#909fab" ))))
- '(gnus-summary-normal-read ((t (:foreground "#909fab"))))
- )
+
+(let ((class '((class color) (min-colors 89)))
+      (fg0    "#bdc3ce")
+      (fg1    "#595e66")
+      (fg2    "#63747c")
+      (fg3    "#909fab")
+      (fg4    "#b894b0")
+      (bg0    "#2a2f38")
+      (bg1    "#e1cb8c") ;; cursor
+      (bg2    "#2b303a")
+      (bg3    "#3c4449")
+      (bg4    "#464a4d")
+      )
+  (custom-theme-set-faces
+   'arjen-grey
+   ;; Built in faces
+   `(default ((,class (:background ,bg0 :foreground ,fg0))))
+   `(cursor ((t (:background ,bg1 ))))
+   `(fringe ((t (:background ,bg2 ))))
+   `(mode-line ((t (:foreground ,fg0 :background ,bg0 ))))
+   `(region ((t (:background ,bg3 ))))
+   `(linum ((t (:foreground  ,fg1  :background ,bg0 ))))
+   `(secondary-selection ((t (:background ,bg4 ))))
+   `(font-lock-builtin-face ((t (:foreground ,bg1 ))))
+   `(font-lock-comment-face ((t (:foreground ,fg2 ))))
+   `(font-lock-function-name-face ((t (:foreground ,fg3 ))))
+   `(font-lock-keyword-face ((t (:foreground ,fg4 ))))
+   `(font-lock-string-face ((t (:foreground "#a8c194" ))))
+   `(font-lock-type-face ((t (:foreground "#a0a5a0" ))))
+   `(font-lock-constant-face ((t (:foreground "#8b9db0" ))))
+   `(font-lock-variable-name-face ((t (:foreground "#8294ac" ))))
+   `(minibuffer-prompt ((t (:foreground "#a8c194" :bold t ))))
+   `(font-lock-warning-face ((t (:foreground "red" :bold t ))))
+   ;; Helm faces
+   `(helm-header ((t (:foreground  ,fg0
+                                   :background ,bg0
+                                   :underline nil
+                                   :box nil))))
+   `(helm-source-header ((t (:foreground  ,fg0
+                                          :background ,bg0
+                                          :underline nil
+                                          :weight bold
+                                          :box (:line-width -1 :style released-button)))))
+   `(helm-ff-directory ((t (:foreground  ,fg0
+                                         :background ,bg0
+                                         :underline nil
+                                         :weight bold
+                                         ))))
+   `(helm-selection ((t (:background "#3c4449" :underline nil))))
+   `(helm-selection-line ((t (:background ,bg0))))
+   ;; Perspective faces
+   `(persp-selected-face ((t (:foreground  ,bg1))))
+   ;; Company mode
+   `(company-tooltip ((t (:foreground ,fg0 :background "#242a34"))))
+   `(company-tooltip-annotation ((t (:foreground ,bg1))))
+   `(company-tooltip-selection ((t (:background "#464a4d"))))
+   `(company-tooltip-mouse ((t (:background "#464a4d"))))
+   `(company-tooltip-common ((t (:foreground ,fg3))))
+   `(company-scrollbar-fg ((t (:background "#464a4d"))))
+   `(company-scrollbar-bg ((t (:background "#242a34"))))
+   `(company-preview ((t (:foreground ,fg0 :background "#242a34"))))
+   `(company-preview-common ((t (:foreground ,fg3))))
+   ;; Gnus
+   `(gnus-header-name ((t (:foreground ,fg3 ))))
+   `(gnus-header-content ((t (:foreground ,fg0 ))))
+   `(gnus-header-subject ((t (:foreground ,bg1 ))))
+   `(widget-button ((t (:foreground ,fg3 ))))
+   `(gnus-summary-normal-read ((t (:foreground ,fg3))))
+   ))
 
 (custom-theme-set-variables
   'arjen-grey
